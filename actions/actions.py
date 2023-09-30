a540a977fbd97a3bc365f755311d4e0c61cd64d3
n <bimbo@prince.com>
2023-09-30 14:28:24 +0200

    feat: intermediate result

--- actions/actions.py
+++ actions/actions.py
@@ -1,20 +1,9 @@
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker, FormValidationAction
-from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 import requests
 from dateutil import parser
-
-class ActionBla(Action):
-
-    def name(self) -> Text:
-        return "action_bla"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        return [SlotSet("number_of_guests", None)]
 
 def extract_value(
         tracker: Tracker, slot_name: str, entity_name: str, duckling_dimension: str,
@@ -31,9 +20,12 @@ def query_duckling(
         text: str,
         dimension: str
 ) -> Any:
-    response = requests.post('http://0.0.0.0:8000/parse', data={"text": text, "dims": [dimension]}).json()
+    response = requests.post('http://0.0.0.0:8000/parse', data={"text": text, "dims": f'["{dimension}"]' }).json()
     if len(response) > 0:
-        return response[0]['value']['value']
+        if 'value' in response[0]['value']:
+            return response[0]['value']['value']
+        elif 'values' in response[0]['value']:
+            return response[0]['value']['values'][0]
     return None
 
 def set_slot_or_show_error(
@@ -59,6 +51,12 @@ class ValidateBookingForm(FormValidationAction):
         self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
     ) -> Dict[Text, Any]:
         value = extract_value(tracker, 'duration_of_stay', 'duration_of_stay', 'number')
+        if value is None and 'from_date' in tracker.slots and tracker.slots['from_date'] is not None:
+            until_date = next(tracker.get_latest_entity_values('until_date'), None)
+            if until_date is not None:
+                until_iso_date = query_duckling(until_date, 'time')
+                if until_iso_date is not None:
+                    value = (parser.isoparse(until_iso_date) - parser.isoparse(tracker.slots['from_date'])).days
         return set_slot_or_show_error(dispatcher, tracker, value, 'duration_of_stay')
 
     async def extract_number_of_guests(
@@ -73,8 +71,8 @@ class ActionBookingSummary(Action):
     def name(self) -> Text:
         return "action_booking_summary"
 
-    def calculate_price(self, duration_in_days: int):
-        return duration_in_days * 100
+    def calculate_price(self, duration_in_days: int, breakfast: bool):
+        return duration_in_days * (110 if breakfast else 100)
 
     def format_date(self, value: str):
         return parser.isoparse(value).date()
@@ -88,9 +86,9 @@ class ActionBookingSummary(Action):
         Number of guests: {tracker.slots['number_of_guests']}\n
         Date of arrival: {self.format_date(tracker.slots['from_date'])}\n
         Duration of stay: {tracker.slots['duration_of_stay']} nights\n
-        With breakfast: {'yes' if tracker.slots['number_of_guests'] else 'no'}\n
+        With breakfast: {'yes' if tracker.slots['breakfast'] else 'no'}\n
         \n
-        Total costs: ${self.calculate_price(int(tracker.slots['duration_of_stay']))}\n
+        Total costs: ${self.calculate_price(int(tracker.slots['duration_of_stay']), tracker.slots['breakfast'])}\n
         \n
         Would you like to confirm your booking? (y/n)"""
         dispatcher.utter_message(text=text)
